b7bd2684d0cbd7a916bf525deecde315ae0ae4c0
obx1 <128701338+obx1@users.noreply.github.com>
2024-12-14 20:49:38 +0100

    User.java

--- Library_Management_System/src/Model/User.java
+++ Library_Management_System/src/Model/User.java
@@ -60,9 +60,9 @@ package Model;
 	    public String toString() {
 	        return "User{" +
 	                "id=" + id +
-	                ", name='" + name + '\'' +
-	                ", email='" + email + '\'' +
-	                ", role=" + getRole() +
+	                "; name='" + name + '\'' +
+	                "; email='" + email + '\'' +
+	                "; role=" + getRole() +
 	                '}';
 	    }
 
