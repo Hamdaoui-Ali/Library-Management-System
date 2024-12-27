d0286001326ec060d82731ce2d6dadfab016c603
obx1 <128701338+obx1@users.noreply.github.com>
2024-12-27 17:18:20 +0100

    Member.java

--- Library_Management_System/src/Model/Member.java
+++ Library_Management_System/src/Model/Member.java
@@ -1,12 +1,15 @@
-package Model;
+package model;
+
+import java.util.List;
 
 public class Member extends User {
-    public Member(int id, String name, String email, String password) {
-        super(id, name, email, password);
-    }
 
-    @Override
-    public String getRole() {
-        return "Member";
-    }
+	public Member(int id, String nom, String prenom, String cin, int age, String mail, String telephone,
+			String adresse) {
+		super(id, nom, prenom, cin, age, mail, telephone, adresse);
+		// TODO Auto-generated constructor stub
+	}
+
+    	
 }
+
