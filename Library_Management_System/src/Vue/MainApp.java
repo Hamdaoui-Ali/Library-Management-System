7821b81d4c1ea644d80ca2245b18b0c9ac2b1630
obx1 <128701338+obx1@users.noreply.github.com>
2024-12-27 17:19:36 +0100

    MainApp.java

--- Library_Management_System/src/Vue/MainApp.java
+++ Library_Management_System/src/Vue/MainApp.java
@@ -1,9 +1,14 @@
 package Vue;
 
+import controller.UserController;
+
 import javax.swing.*;
 
 public class MainApp {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginView());
+        SwingUtilities.invokeLater(() -> {
+            UserController userController = new UserController(); // Initialisation du contrôleur
+            new MenuView(userController); // Passer le contrôleur au menu principal
+        });
     }
 }
