e761c45ea3e5d266dc6ab4d58423d8bd184a7947
obx1 <128701338+obx1@users.noreply.github.com>
2024-12-27 17:17:36 +0100

    Admin.java

--- Library_Management_System/src/Model/Admin.java
+++ Library_Management_System/src/Model/Admin.java
@@ -1,14 +1,15 @@
-package Model;
+package model;
 
+import java.util.Arrays;
+import java.util.List;
 
 public class Admin extends User {
-    public Admin(int id, String name, String email, String password) {
-        super(id, name, email, password);
-    }
 
-    @Override
-    public String getRole() {
-        return "Admin";
+	public Admin(int id, String nom, String prenom, String cin,int age,String mail, String telephone,
+			String adresse) {
+		super(id, nom, prenom, cin, age,mail, telephone, adresse);
+		// TODO Auto-generated constructor stub
+	}
+    
     }
-}
 
